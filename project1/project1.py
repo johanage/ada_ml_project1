0896fe594850d3d16a322f6ccf0803f1e85000c9
johanage <=>
2022-10-11 23:53:02 +0200

    last update for today, fixed bug with kfold cv

--- project1/project1.py
+++ project1/project1.py
@@ -80,14 +80,13 @@ def ols_fp_train_test_split(X, y, **kwargs):
     ytilde_test = Xtest@betahat + np.mean(ytrain)
     return ytilde_train, ytilde_test, betahat, Xtrain, Xtest, ytrain,ytest
 
-def center_X(X):
-    for j in range(X.shape[1]):
-        X[j] -= - np.mean(X[:,j], axis=0)
-    return X
+def scale_center_X(X):
+    return (X - np.mean(X, axis=0))/np.std(X, axis=0)
 
-def ridge_fp_wo_split(X, y, lmbda, **kwargs):
+def ridge_fp_wo_split(X, y, lmbda, centering = False, **kwargs):
     # centering inputs
-    X = center_X(X=X)
+    if centering == True:
+        X = center_X(X=X)
     # computing beta params with train set
     p = X.shape[1]
     A = np.linalg.inv(X.T@X + np.eye(p, p)*lmbda)@X.T
@@ -96,7 +95,7 @@ def ridge_fp_wo_split(X, y, lmbda, **kwargs):
     ytilde = X@betahat
     return ytilde, betahat
 
-def ridge_fp_train_test_split(X, y, lmbda, **kwargs):
+def ridge_fp_train_test_split(X, y, lmbda, centering = False, **kwargs):
     """
     X has len=p not len=p+1 (cols with 1s is removed)
 
@@ -104,7 +103,8 @@ def ridge_fp_train_test_split(X, y, lmbda, **kwargs):
     if np.sum(X[:,0] == np.ones(X.shape[1])) == X.shape[1]:
         raise ValueError(" First column of the design matrix needs to be removed")
     # centering inputs
-    X = center_X(X=X)
+    if centering == True:
+        X = center_X(X=X)
     # split into training and test sets
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, ycentered, **kwargs)
     # compute predictor for training and test set and add intercept
@@ -114,16 +114,21 @@ def ridge_fp_train_test_split(X, y, lmbda, **kwargs):
     return ytilde_train, ytilde_test, betahat, Xtrain, Xtest, ytrain,ytest
 
 from sklearn import linear_model
-def lasso_fp_wo_split(X, y, lmbda, **kwargs):
+def lasso_fp_wo_split(X, y, lmbda, centering = False, **kwargs):
+    # centering inputs
+    if centering == True:
+        X = center_X(X=X)
     clf = linear_model.Lasso(alpha = lmbda)
     clf.fit(X = X, y = y-np.mean(y))
     betahat = clf.coef_
     ytilde = X@betahat + np.mean(y)
     return ytilde, betahat
 
-def lasso_fp_train_test_split(X, y, lmbda, **kwargs):
-    ycentered = y - np.mean(y)
-    Xtrain, Xtest, ytrain, ytest = train_test_split(X, ycentered, **kwargs)
+def lasso_fp_train_test_split(X, y, lmbda, centering = False, **kwargs):
+    # centering inputs
+    if centering == True:
+        X = center_X(X=X)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, **kwargs)
     clf = linear_model.Lasso(alpha = lmbda)
     clf.fit(X = Xtrain, y = ytrain)
     betahat = clf.coef_
@@ -146,7 +151,7 @@ def bias(y, ytilde):
 
 def resample(data):
     n = len(data.ravel())
-    return data.ravel()[np.random.randint(0,n,n)]
+    return data.ravel()[np.random.randint(0,n,n)].reshape(data.shape)
 
 def bootstrap(data, k, keys_ops = {'mean' : np.mean, 'var' : np.var}):
     stats = {key : np.zeros(k) for key in keys_ops.keys()}
@@ -157,12 +162,13 @@ def bootstrap(data, k, keys_ops = {'mean' : np.mean, 'var' : np.var}):
     return stats
 
 from sklearn import linear_model
-def cross_validation(data, xvec, k, p, method, lmbda = None):
+def cross_validation(data, xvec, k, p, method, lmbda = None, scale_centering = False, scale = False, factor=1):
     """
     Args:
     data - data to be fitted, ndarray
     k - number of folds, int
     p - polynomial degree, int
+    centering - centering the design matrix, bool 
 
     Out:
     mses - MSEs for each train test pair, np array with dim = (k,)
@@ -176,7 +182,6 @@ def cross_validation(data, xvec, k, p, method, lmbda = None):
     isplit = np.full(k, len(b.ravel()) // k, dtype = int)
     isplit[:len(b.ravel()) % k] += 1
     isplit = np.cumsum(isplit)
-    # shuffle the indices for random selection of datapoints
     np.random.shuffle(indices)
     # split the datasets in k folds
     splits = np.split(np.arange(len(b.ravel()))[indices], isplit[:-1])
@@ -193,18 +198,28 @@ def cross_validation(data, xvec, k, p, method, lmbda = None):
         test = np.array(test,dtype=int)
         train = np.concatenate(np.delete(splits, itest, axis=0))
         train = np.array(train, dtype=int)
-
-        #Xtrain = make_design_matrix(xvec = np.array([x.ravel()[train] for x in xvec]), p = p)
-        Xtrain = create_X(x = xvec[0].ravel()[train], y = xvec[1].ravel()[train], n = p)
-        #Xtest = make_design_matrix(xvec = np.array([x.ravel()[test] for x in xvec]), p = p)
-        Xtest = create_X(x = xvec[0].ravel()[test], y = xvec[1].ravel()[test], n = p)
+        if len(xvec) == 1:
+            X = np.zeros((len(xvec[0]), p+1))
+            X[:,0] = 1
+            for deg in range(1,p+1):
+                X[:,deg] = xvec[0]**deg
+            Xtrain = X[train]
+            Xtest = X[test]
+        if len(xvec) == 2:
+            #Xtrain = make_design_matrix(xvec = np.array([x.ravel()[train] for x in xvec]), p = p)
+            Xtrain = create_X(x = xvec[0].ravel()[train], y = xvec[1].ravel()[train], n = p)
+            #Xtest = make_design_matrix(xvec = np.array([x.ravel()[test] for x in xvec]), p = p)
+            Xtest = create_X(x = xvec[0].ravel()[test], y = xvec[1].ravel()[test], n = p)
         if method == "ols":
-            data_tilde_train, betahat = ols_fp_wo_split(X = Xtrain, y = b[train] - np.mean(b[train]))
-            data_tilde_train += np.mean(b[train])
-            data_tilde_test = Xtest@betahat + np.mean(b[train])
+            data_tilde_train, betahat = ols_fp_wo_split(X = Xtrain, y = b[train])
+            data_tilde_test = Xtest@betahat
         if method == "lasso":
             Xtrain = Xtrain[:,1:]
-            Xtest =  Xtest[:,1:]
+            if scale_centering == True:
+                mean_Xtrain = np.mean(Xtrain, axis=0)
+                std_Xtrain = np.std(Xtrain, axis = 0)
+                Xtest =  (Xtest[:,1:] - mean_Xtrain)/std_Xtrain
+                Xtrain = scale_center_X(X=Xtrain)
             if lmbda is None:
                 raise ValueError("Lambda value has not been set")
             clf_train = linear_model.Lasso(alpha = lmbda)
@@ -214,11 +229,16 @@ def cross_validation(data, xvec, k, p, method, lmbda = None):
             data_tilde_test = Xtest@betahat + np.mean(b[train])
         if method == "ridge":
             Xtrain = Xtrain[:,1:]
-            Xtest =  Xtest[:,1:]
+            if scale_centering == True:
+                mean_Xtrain = np.mean(Xtrain, axis=0)
+                std_Xtrain = np.std(Xtrain,axis=0)
+                Xtest =  (Xtest[:,1:]-mean_Xtrain)/std_Xtrain  
+                Xtrain = scale_center_X(X=Xtrain)
             if lmbda is None:
                 raise ValueError("Lambda value has not been set")
             data_tilde_train, betahat = ridge_fp_wo_split(X=Xtrain, y=b[train]-np.mean(b[train]), lmbda=lmbda)
-            data_tilde_train += np.mean(b[train])
+            data_tilde_train = (data_tilde_train + np.mean(b[train]))
+            data_tilde_test = Xtest@betahat + np.mean(b[train])
             data_tilde_test = Xtest@betahat + np.mean(b[train])
         mses_train[itest] = MSE(y = b[train], ytilde = data_tilde_train)
         mses_test[itest]  = MSE(y = b[test], ytilde = data_tilde_test)
